eefe526bc82b263fada31020dd7a3289f2019024
bkomac <borut@komac.si>
2019-09-17 23:07:17 +0200

    cash

--- server.js
+++ server.js
@@ -1,41 +1,58 @@
-var app        = require('express')();
+var app = require('express')();
 var bodyParser = require('body-parser');
-var http       = require('http').Server(app);
-var io         = require('socket.io')(http);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+global.lastLocations = [];
 
 // parse application/json
-app.use(bodyParser.json({ type : '*/*' })); // force json
+app.use(bodyParser.json({ type: '*/*' })); // force json
 
-app.all('/*', function(req, res, next) {
+app.all('/*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+
+  for (var i = 0; i < lastLocations.length; i++) {
+    console.log('---:' + i);
+  }
+
+  setTimeout(function () {
+    console.log('emit :' + JSON.stringify(lastLocations));
+    io.emit('locations', lastLocations);
+  }, 2000);
+  
 });
 
-app.post('/locations', function(request, response){
-    console.log('Headers:\n', request.headers);
-    console.log('Locations:\n', request.body);
-    console.log('------------------------------');
-    io.emit('locations', request.body);
-    response.sendStatus(200);
+app.post('/locations', function (request, response) {
+  console.log('Headers:\n', request.headers);
+  console.log('Locations:\n', request.body);
+  console.log('------------------------------', request.body[0].user);
+
+  lastLocations[request.body[0].user.uuid] = request.body;
+
+  console.log('Last loc :' + JSON.stringify(lastLocations[request.body[0].user.uuid]));
+
+  io.emit('locations', request.body);
+  response.sendStatus(200);
 });
 
-app.post('/sync', function(request, response){
-    console.log('Headers:\n', request.headers);
-    console.log('Synced Locations:\n', request.body);
-    console.log('------------------------------');
-    io.emit('locations', request.body);
-    response.sendStatus(200);
+app.post('/sync', function (request, response) {
+  console.log('Headers:\n', request.headers);
+  console.log('Synced Locations:\n', request.body);
+  console.log('------------------------------');
+  io.emit('locations', request.body);
+  response.sendStatus(200);
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
